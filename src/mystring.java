c3d3f1a148a4ea98a5ddbca0880749155becf519
ShaikahAlghareeb <s2221173949@ku.edu.kw>
2026-09-23 18:21:50 +0300

    I changed a line by adding else

--- src/mystring.java
+++ src/mystring.java
@@ -1,9 +1,32 @@
 
 public class mystring {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+		
+			private String Text;
+			public mystring() {
+			Text = null;
+			}
+			
+			public void setString(String t){
+			Text = t;
+			}
+			
+			public String invert( ){
+			int i , n = Text.length();
+			
+			char [] Temp;
+			Temp = Text.toCharArray();
+			Text=
+			"";
+			for ( i=0; i<n; i++) {
+				
+			if (Temp[i] >= 'A' && Temp[i] <= 'Z')
+			Temp[i]= (char)(Temp[i] - 'A' + 'a') ;
+			else if (Temp[i] >= 'a' && Temp[i] <= 'z')
+			Temp[i]= (char)(Temp[i] - 'a' + 'A') ;
+			
+			Text += Temp[i];
+			}
+			return Text;
+			}
+			}
+	
